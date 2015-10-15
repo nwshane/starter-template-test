771816847a1d5f0c949d989c37bc816cc46e8b7a
nwshane <django09@gmail.com>
2015-10-15 11:52:48 +0400

    Changed repository location.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :domain, 'alpha.jumpstart.ge'
 set :user, 'prisoners-staging'
 set :application, 'Starter-Template-Staging'
-set :repository, 'git@github.com:JumpStartGeorgia/Starter-Template.git'
+set :repository, 'git@github.com:nwshane/starter-template-test.git'
 set :web_url, 'dev-prisoners.jumpstart.ge'
 set :visible_to_robots, false
